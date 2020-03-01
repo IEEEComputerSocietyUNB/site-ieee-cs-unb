2a51b192e5a3148f24aa328b80710c11a0b5f879
Christyan David <christyan.david@outlook.com>
2020-03-01 19:08:37 -0300

    adiciona includes js na pagina index e about

---
+++ includes/conviteCS.js
@@ -0,0 +1,12 @@
+document.write(`
+<div class="container">
+<div class="row">
+<div class="col-lg-8 cta-left">
+<h1>Quer fazer parte da Computer Society?</h1>
+</div>
+<div class="col-lg-4 cta-right">
+<a class="primary-btn wh" href="#">Aplique-se!</a>
+</div>
+</div>
+</div>
+`);
---
+++ includes/footer.js
@@ -0,0 +1,70 @@
+document.write(`
+<div class="container">
+<div class="row">
+<div class="col-lg-2 col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Links</h4>
+<ul>
+<li><a href="http://www.ieee.org.br/">IEEE Brasil</a></li>
+<li><a href="#">Voluntários</a></li>
+<li><a href="#">Processo Seletivo</a></li>
+<li><a href="#">Galeria</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-2 col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Eventos</h4>
+<ul>
+<li><a href="#">Semana Universitária</a></li>
+<li><a href="#">IEEExtreme</a></li>
+<li><a href="#">Outro evento</a></li>
+<li><a href="#">Outro evento 2</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-4 col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Projetos</h4>
+<ul>
+<li><a href="#">Rabot</a></li>
+<li><a href="#">Coding Dojo</a></li>
+</ul>
+</div>
+</div>
+<div class="col-lg-4  col-md-6 col-sm-6">
+<div class="single-footer-widget">
+<h4>Newsletter</h4>
+<p>Fique sabendo dos nossos eventos!</p>
+<div class="" id="mc_embed_signup">
+<form target="_blank" action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01" method="get">
+<div class="input-group">
+<input type="text" class="form-control" name="EMAIL" placeholder="Enter Email Address" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter Email Address '" required="" type="email">
+<div class="input-group-btn">
+<button class="btn btn-default" type="submit">
+<span class="lnr lnr-arrow-right"></span>
+</button>
+</div>
+<div class="info"></div>
+</div>
+</form>
+</div>
+</div>
+</div>
+</div>
+<div class="footer-bottom row align-items-center justify-content-between">
+<p class="footer-text m-0 col-lg-6 col-md-12">
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;
+<script>document.write(new Date().getFullYear());</script> All rights reserved IEEE Computer Society UnB | This
+template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com"
+target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+</p>
+<div class="col-lg-6 col-sm-12 footer-social">
+<a href="#"><i class="fa fa-facebook"></i></a>
+<a href="#"><i class="fa fa-twitter"></i></a>
+</div>
+</div>
+</div>
+`);
